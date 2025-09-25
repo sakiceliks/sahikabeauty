c4f46779fcc2b03747e71295c282d9ca55fc7400
Saki Celik <87222451+sakiceliks@users.noreply.github.com>
2025-09-25 15:21:07 +0300

    vb

--- components/Header.jsx
+++ components/Header.jsx
@@ -91,7 +91,7 @@ const Header = () => {
           >
             <Link href="/" aria-label="Sahika Beauty anasayfaya git">
               <Image 
-                src="/assets/logo.svg" 
+                src="/sahikalogo.png" 
                 width={100} 
                 height={36} 
                 sizes="(max-width: 640px) 80px, (max-width: 768px) 100px, 120px"
